c37af448e8338bb73165217376b4c6c4792b7fa1
Usenmfon <usenmfonuko@gmail.com>
2023-05-01 23:04:04 +0100

    fix: imported cloudinary module into gallery

--- src/gallery/gallery.module.ts
+++ src/gallery/gallery.module.ts
@@ -3,10 +3,12 @@ import { GalleryService } from './gallery.service';
 import { GalleryController } from './gallery.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Gallery, GallerySchema } from './schema/gallery.schema';
+import { CloudinaryModule } from 'src/helper/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Gallery.name, schema: GallerySchema }]),
+    CloudinaryModule,
   ],
   controllers: [GalleryController],
   providers: [GalleryService],
